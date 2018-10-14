e5340271a24d0d1a96a159397665fdaba795289b
Chris Laupama <chrislaupama@live.com>
2018-10-14 16:26:12 +1300

    tried to get loops working properly, not working... so need to move on for now

--- js.js
+++ js.js
@@ -1,5 +1,19 @@
-$("#btnloop").click(function(){
-    var player = $("#player")[0];
-    if (player.loop == false) {player.loop = true}
-    else {player.loop = false};
-});
+
+/* 
+var music = new Howl({
+    urls: ['samples/loop1.mp3'],
+    autoplay: false,
+    loop: true
+  });
+  var mute = false;
+
+  document.getElementById("music").onclick = function () {
+    if (mute === true) {
+      music.play();
+      mute = false;
+    } else {
+      music.pause();
+      mute = true;
+    }
+  };
+  */
